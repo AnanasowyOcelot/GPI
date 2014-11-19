def7cd4d786d74a6a3d527d17051824ec686755e
AnanasowyOcelot <lukasz.woroniecki@gmail.com>
2014-11-19 13:22:41 +0100

    classificationBundle overriden

---
+++ src/GPI/Sonata/ClassificationBundle/Admin/CategoryAdmin.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Sonata project.
+ *
+ * (c) Sonata Project <https://github.com/sonata-project/SonataClassificationBundle/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace GPI\Sonata\ClassificationBundle\Admin;
+
+class CategoryAdmin extends \Sonata\ClassificationBundle\Admin\CategoryAdmin
+{
+//    protected $formOptions = array(
+//        'cascade_validation' => false,
+//            'inherit_data' => true
+//    );
+
+
+}
