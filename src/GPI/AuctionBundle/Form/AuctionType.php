595ee03c9efe06acf937521d0fc612d779878f02
AnanasowyOcelot <lukasz.woroniecki@gmail.com>
2014-12-10 13:57:04 +0100

    tree structure categories form type

--- src/GPI/AuctionBundle/Form/AuctionType.php
+++ src/GPI/AuctionBundle/Form/AuctionType.php
@@ -2,14 +2,52 @@
 
 namespace GPI\AuctionBundle\Form;
 
+use GPI\Sonata\ClassificationBundle\Entity\CategoryRepository;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\ChoiceList\ChoiceList;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Functional as F;
 
 class AuctionType extends AbstractType
 {
+    /**
+     * @var CategoryRepository
+     */
+    private $catRepo;
+
+    private function categoryTreeMap($tree, $depth = 0)
+    {
+        $treeMap = array();
+        foreach ($tree as $node) {
+            $treeMap[] = array(
+                'label' => str_repeat('-', $depth) . ' ' . $node['name'],
+                'entity' => $node['entity']
+            );
+            $treeMap = array_merge(
+                $treeMap,
+                $this->categoryTreeMap($node['children'], $depth + 1)
+            );
+        }
+        return $treeMap;
+    }
+
+    /**
+     * @return ChoiceList
+     */
+    private function categoryChoiceList()
+    {
+        $tree = $this->catRepo->auctionCategoryTree();
+        $treeMap = $this->categoryTreeMap($tree);
+        return new ChoiceList(
+            F\pluck($treeMap, 'entity'),
+            F\pluck($treeMap, 'label')
+        );
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+
         $builder
             ->add('name', 'text')
             ->add('content', 'textarea')
@@ -18,6 +56,7 @@ class AuctionType extends AbstractType
                 'entity',
                 array(
                     'class' => 'Application\Sonata\ClassificationBundle\Entity\Category',
+                    'choice_list' => $this->categoryChoiceList()
                 )
             )
             ->add(
@@ -44,5 +83,13 @@ class AuctionType extends AbstractType
                 'data_class' => 'GPI\CoreBundle\Model\Auction\AddNewAuctionCommand',
             )
         );
+    }
+
+    /**
+     * @param CategoryRepository $catRepo
+     */
+    public function __construct(CategoryRepository $catRepo)
+    {
+        $this->catRepo = $catRepo;
     }
 }
--- src/GPI/Sonata/ClassificationBundle/Entity/CategoryRepository.php
+++ src/GPI/Sonata/ClassificationBundle/Entity/CategoryRepository.php
@@ -35,7 +35,8 @@ class CategoryRepository extends EntityRepository
             $tree[] = array(
                 'name' => $category->getName(),
                 'children' => $this->auctionCategoryTree($category->getId()),
-                'slug' => $category->getSlug()
+                'slug' => $category->getSlug(),
+                'entity' => $category
             );
         }
 
